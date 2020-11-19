5beed340146ec3cb28878d3011922a1cc7f358e0
zhangtravis <popcorn20020806@gmail.com>
2020-11-19 11:04:56 -0700

    Fixed photo and active tabs

--- js/images.js
+++ js/images.js
@@ -33,9 +33,9 @@ var header = document.getElementById("bttns");
 var btns = header.getElementsByClassName("btn");
 for (var i = 0; i < btns.length; i++) {
   btns[i].addEventListener("click", function() {
-    var current = document.getElementsByClassName("active");
-    current[0].className = current[0].className.replace(" active", "");
-    this.className += " active";
+    var current = document.getElementsByClassName("clicked");
+    current[0].className = current[0].className.replace(" clicked", "");
+    this.className += " clicked";
   });
 }
 
